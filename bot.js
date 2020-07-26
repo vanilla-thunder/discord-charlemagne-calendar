e6055eff8b55224460359712c6e7e1a58d6e21ea
Marat <1874024+vanilla-thunder@users.noreply.github.com>
2020-07-26 18:17:13 +0200

    env debug var fixed

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ var cfg = {};
 if(process.env.discordToken && process.env.syncEndpoint && process.env.deletingEndpoint && process.env.customToken && process.env.debug )
 {
 	cfg = {
-		"debug" : process.env.debug,
+		"debug" : ( process.env.debug == "true" ? true : flase),
 		"discordToken": process.env.discordToken,
 		"customToken": process.env.customToken,
 		"syncEndpoint": process.env.syncEndpoint, 
